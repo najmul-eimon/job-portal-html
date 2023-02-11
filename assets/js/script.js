633fa77c5bd33bcc039cf03216355d9532eabbbc
najmul-eimon <najmul.eimon@intelli.global>
2023-02-11 18:03:24 +0600

    menubar,banner added

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+  "use strick";
+
+  /*======================== banner road animation ===========================*/
+  // const element = document.getElementsByClassName('road');
+
+  // for(let i = 0; i < element.length; i++){
+  //   setInterval(() => {
+  //     element[i].style.animationName = "none";
+  //     setInterval(() => {
+  //       element[i].style.animationName = "road";
+  //     }, 100)
+  //   }, 6000);
+  // }
+
+});
